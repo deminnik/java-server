823c55c7d110f94f4170226378f4de7a97ff9848
deminnik <n.demin1@alumni.nsu.ru>
2020-10-29 18:55:36 +0700

    Add servlet and configure server

--- src/main/java/deminnik/javaserver/Main.java
+++ src/main/java/deminnik/javaserver/Main.java
@@ -1,4 +1,20 @@
 package deminnik.javaserver;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestsServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
 }
